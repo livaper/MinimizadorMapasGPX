684fb9901f08df21e12ac46561a8818ad246fe8d
Livia Pereira <livaper@gmail.com>
2015-12-09 22:34:22 -0200

    Implementacao de metodo para adicao de distancia a reta de pontos adjacentes em cada pontoMarcado de uma trajetoria

--- src/Trajetoria.java
+++ src/Trajetoria.java
@@ -1,8 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Classe de domínio para a trajetória realizada no arquivo GPX
+ * 
  * @author liviapereira
  *
  */
 public class Trajetoria {
+	List<PontoMarcado> pontosMarcados;
+
+	public Trajetoria() {
+		super();
+		pontosMarcados = new ArrayList<PontoMarcado>();
+	}
+
+	/**
+	 * ADICIONA A DISTANCIA DE UM PONTO ATE A RETA FORMADA PELOS SEUS ADJACENTES
+	 */
+	public void adicionaDistanciaARetaAdjacenteDosPontos() {
+		CalculadoraDistanciaPontoReta calculadora = new CalculadoraDistanciaPontoReta();
+
+		for (PontoMarcado ponto : pontosMarcados) {
+			boolean primeiroPontoTrajetoria = pontosMarcados.indexOf(ponto) == 0;
+			boolean ultimoPontoTrajetoria = pontosMarcados.indexOf(ponto) == (pontosMarcados.size() - 1);
+
+			if (!primeiroPontoTrajetoria && !ultimoPontoTrajetoria) {
+				PontoMarcado pontoSeguinte = pontosMarcados.get(pontosMarcados.indexOf(ponto) + 1);
+				PontoMarcado pontoAnterior = pontosMarcados.get(pontosMarcados.indexOf(ponto) - 1);
+				Double distancia = calculadora.calculaDistanciaDeUmPontoAReta(ponto, pontoAnterior, pontoSeguinte);
+				ponto.setDistanciaARetaAdjacente(distancia);
+			}
+		}
+	}
+
+	public List<PontoMarcado> getPontosMarcados() {
+		return pontosMarcados;
+	}
+
+	public void setPontosMarcados(List<PontoMarcado> trajetoria) {
+		this.pontosMarcados = trajetoria;
+	}
 
 }
