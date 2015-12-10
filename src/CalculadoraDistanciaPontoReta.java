cf460bbc6c8f05e5adeced9dc565a024b4579c25
Livia Pereira <livaper@gmail.com>
2015-12-09 22:35:58 -0200

    Criacao de classe calculadora da distancia de um ponto a uma reta

---
+++ src/CalculadoraDistanciaPontoReta.java
@@ -0,0 +1,18 @@
+
+public class CalculadoraDistanciaPontoReta {
+
+	/**
+	 * CALCULA A DISTANCIA DE "ponto" ATE A RETA FORMADA "pontoAnterior" ao
+	 * "pontoSeguinte"
+	 * 
+	 * @param ponto
+	 * @param pontoAnterior
+	 * @param pontoSeguinte
+	 * @return
+	 */
+	public Double calculaDistanciaDeUmPontoAReta(PontoMarcado ponto, PontoMarcado pontoAnterior,
+			PontoMarcado pontoSeguinte) {
+		// TODO
+		return 0.0;
+	}
+}
