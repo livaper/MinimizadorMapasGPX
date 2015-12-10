b2b00559602bfb96f594e4aa8e294d2f912377f6
Livia Pereira <livaper@gmail.com>
2015-12-10 15:19:45 -0200

    Implementacao da calculadora da distancia de um ponto a reta formada pelos pontos adjacentes numa trajetoria

--- src/CalculadoraDistanciaPontoReta.java
+++ src/CalculadoraDistanciaPontoReta.java
@@ -2,17 +2,24 @@
 public class CalculadoraDistanciaPontoReta {
 
 	/**
-	 * CALCULA A DISTANCIA DE "ponto" ATE A RETA FORMADA "pontoAnterior" ao
-	 * "pontoSeguinte"
+	 * CALCULA A DISTANCIA DE um "ponto" ATE A RETA FORMADA PELO "pontoAnterior"
+	 * E O "pontoSeguinte"
 	 * 
 	 * @param ponto
 	 * @param pontoAnterior
 	 * @param pontoSeguinte
-	 * @return
+	 * @return distancia
 	 */
 	public Double calculaDistanciaDeUmPontoAReta(PontoMarcado ponto, PontoMarcado pontoAnterior,
 			PontoMarcado pontoSeguinte) {
-		// TODO
-		return 0.0;
+		Double a, b, c, distancia, x3, y3;
+		a = (pontoAnterior.getLatitude() - pontoSeguinte.getLatitude())
+				/ (pontoAnterior.getLongitude() - pontoSeguinte.getLongitude());
+		b = pontoSeguinte.getLatitude() - (a * pontoSeguinte.getLongitude());
+		c = (a * ponto.getLongitude()) + ponto.getLatitude();
+		x3 = (c - b) / (2 * a);
+		y3 = (a * x3) + b;
+		distancia = Math.sqrt(Math.pow((ponto.getLongitude() - x3), 2) + Math.pow((ponto.getLatitude() - y3), 2));
+		return distancia;
 	}
 }
