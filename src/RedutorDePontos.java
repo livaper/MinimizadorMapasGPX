837cf8d3ca6e9ecd3958df265a8721e1e0543b10
Livia Pereira <livaper@gmail.com>
2015-12-09 22:32:52 -0200

    Implementacao da estrutura da classe de dominio RedutorDePontos

--- src/RedutorDePontos.java
+++ src/RedutorDePontos.java
@@ -1,8 +1,31 @@
+import java.util.List;
+
 /**
  * Classe que aplica a lógica de redução de pontos numa trajetória
+ * 
  * @author liviapereira
  *
  */
 public class RedutorDePontos {
 
+	/**
+	 * APLICA A REDUCAO DE PONTOS EM UMA TRAJETORIA CONSIDERANDO RETIRANDO X%
+	 * DOS PONTOS MAIS PROXIMOS DE UMA RETA FORMADA PELOS PONTOS ADJACENTES
+	 * 
+	 * @param trajetoriaOriginal
+	 *            - trajetoria lida pelo Leitor GPX
+	 * @param porcentagem
+	 *            - valor entre 0 e 100, inclusive.
+	 */
+	public void reduzTrajetoriaPorPorcentagem(Trajetoria trajetoriaOriginal, Double porcentagem) {
+
+
+	}
+
+	
+
+
+	public void reduzTrajetoriaPorDistancia(Trajetoria trajetoriaOriginal, Double distancia) {
+
+	}
 }
